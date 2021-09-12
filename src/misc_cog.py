5fcd6f71d6bb7d0194d3e356b12672576bb04df6
AaronTheNerd <bargeaaron@gmail.com>
2021-09-12 19:36:03 -0400

    Made cog member functions private

--- src/misc_cog.py
+++ src/misc_cog.py
@@ -1,16 +1,14 @@
-import discord
 import randfacts
 from discord.ext import commands
-
 
 class MiscCog(commands.Cog, name="Miscellaneous"):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name='hello', aliases=['hey', 'howdy'])
-    async def hello(self, ctx):
+    async def _hello(self, ctx):
         await ctx.send(f'Hello, {ctx.author}!')
 
     @commands.command(name='fact', aliases=['randfact', 'rf'])
-    async def fact(self, ctx):
+    async def _fact(self, ctx):
         await ctx.send(f"Did you know?\n{randfacts.get_fact()}")
