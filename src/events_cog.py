437d771e263973a4f27fc1136212edcb729b7312
AaronTheNerd <bargeaaron@gmail.com>
2021-09-12 19:35:44 -0400

    Added ability to insult the user in old english rarely

--- src/events_cog.py
+++ src/events_cog.py
@@ -1,6 +1,7 @@
+import random
 from discord.ext import commands
 import discord
-from config import COG_CONFIGS
+from config import ROLE_ON_JOIN_CONFIGS, RANDOM_INSULT_CONFIGS
 
 class EventsCog(commands.Cog, name="Events"):
     def __init__(self, bot):
@@ -12,5 +13,12 @@ class EventsCog(commands.Cog, name="Events"):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        role = discord.utils.get(member.guild.roles, name=COG_CONFIGS["events"]["default_role"])
-        await member.add_roles(role)
+        if ROLE_ON_JOIN_CONFIGS["enabled"]:
+            role = discord.utils.get(member.guild.roles, name=ROLE_ON_JOIN_CONFIGS["role"])
+            await member.add_roles(role)
+
+    @commands.Cog.listener()
+    async def on_command(self, ctx):
+        if RANDOM_INSULT_CONFIGS['enabled'] and random.random() < RANDOM_INSULT_CONFIGS["insult_chance"]:
+            insult = f'You\'re a {(random.choice(RANDOM_INSULT_CONFIGS["adjectives"]) + ", ") if random.random() < RANDOM_INSULT_CONFIGS["adjective_chance"] else ""} {random.choice(RANDOM_INSULT_CONFIGS["insults"])}'
+            await ctx.send(insult, delete_after=RANDOM_INSULT_CONFIGS["delete_after"])
