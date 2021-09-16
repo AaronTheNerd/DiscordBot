f8f042a6c92fe6198ba55c1d1375ceb6cf6a7c7d
AaronTheNerd <bargeaaron@gmail.com>
2021-09-16 13:16:04 -0400

    Added music bot code from vbe0201

--- src/bot.py
+++ src/bot.py
@@ -1,7 +1,17 @@
 """Written by Aaron Barge
 Copyright 2021
 
-Module for any intializations needed for the discord bot
+Module for any intializations needed for the discord bot.
+Permissions:
+    Text:
+        Send Messages
+        Embed Links
+        Add Reactions
+    Voice:
+        Connect
+        Speak
+        Use Voice Activity
+Permission Integer: 36718656
 """
 
 from discord.ext import commands
@@ -11,7 +21,7 @@ from config import DISCORD_CONFIGS, MISC_COG_CONFIGS, SPOTIFY_COG_CONFIGS, EVENT
 from cogs.events_cog import EventsCog
 from cogs.misc_cog import MiscCog
 from cogs.spotify_cog import SpotifyCog
-from cogs.youtube_cog import YoutubeCog
+from cogs.youtube_cog import Music
 
 bot = commands.Bot(command_prefix=DISCORD_CONFIGS["command_prefix"])
 
@@ -25,7 +35,7 @@ if EVENTS_COG_CONFIGS["enabled"]:
     bot.add_cog(EventsCog(bot))
 
 if YOUTUBE_COG_CONFIGS["enabled"]:
-    bot.add_cog(YoutubeCog(bot))
+    bot.add_cog(Music(bot))
 
 if __name__ == "__main__":
     bot.run(DISCORD_CONFIGS["token"])
