51aa4331e96fc79ac819abbc6ed9eae306d5de3a
AaronTheNerd <bargeaaron@gmail.com>
2021-09-17 19:42:24 -0400

    Refactored code

--- src/cogs/dnd_cog.py
+++ src/cogs/dnd_cog.py
@@ -20,79 +20,37 @@ class DnDCog(commands.Cog, name="DnD"):
     @commands.command(name='d4')
     async def _d4(self, ctx, num_of_rolls: int = 1):
         """Rolls a 4-sided dice."""
-        rolls = []
-        for i in range(num_of_rolls):
-            rolls.append(random.randint(1, 4))
-        rolls_str = f"Rolls: {str(rolls)}"
-        if len(rolls_str) < 2000:
-            await ctx.send(rolls_str)
-        await ctx.send(f"{'Total' if num_of_rolls != 1 else 'Rolled'}: {sum(rolls)}")
+        await ctx.invoke(self._roll, 4, num_of_rolls)
 
     @commands.command(name='d6')
-    async def _d6(self, ctx, *, num_of_rolls: int = 1):
+    async def _d6(self, ctx, num_of_rolls: int = 1):
         """Rolls a 6-sided dice."""
-        rolls = []
-        for i in range(num_of_rolls):
-            rolls.append(random.randint(1, 6))
-        rolls_str = f"Rolls: {str(rolls)}"
-        if len(rolls_str) < 2000:
-            await ctx.send(rolls_str)
-        await ctx.send(f"{'Total' if num_of_rolls != 1 else 'Rolled'}: {sum(rolls)}")
+        await ctx.invoke(self._roll, 6, num_of_rolls)
 
     @commands.command(name='d8')
-    async def _d8(self, ctx, *, num_of_rolls: int = 1):
+    async def _d8(self, ctx, num_of_rolls: int = 1):
         """Rolls an 8-sided dice."""
-        rolls = []
-        for i in range(num_of_rolls):
-            rolls.append(random.randint(1, 8))
-        rolls_str = f"Rolls: {str(rolls)}"
-        if len(rolls_str) < 2000:
-            await ctx.send(rolls_str)
-        await ctx.send(f"{'Total' if num_of_rolls != 1 else 'Rolled'}: {sum(rolls)}")
+        await ctx.invoke(self._roll, 8, num_of_rolls)
 
     @commands.command(name='d10')
-    async def _d10(self, ctx, *, num_of_rolls: int = 1):
+    async def _d10(self, ctx, num_of_rolls: int = 1):
         """Rolls a 10-sided dice."""
-        rolls = []
-        for i in range(num_of_rolls):
-            rolls.append(random.randint(1, 10))
-        rolls_str = f"Rolls: {str(rolls)}"
-        if len(rolls_str) < 2000:
-            await ctx.send(rolls_str)
-        await ctx.send(f"{'Total' if num_of_rolls != 1 else 'Rolled'}: {sum(rolls)}")
+        await ctx.invoke(self._roll, 10, num_of_rolls)
 
     @commands.command(name='d12')
-    async def _d12(self, ctx, *, num_of_rolls: int = 1):
+    async def _d12(self, ctx, num_of_rolls: int = 1):
         """Rolls a 12-sided dice."""
-        rolls = []
-        for i in range(num_of_rolls):
-            rolls.append(random.randint(1, 12))
-        rolls_str = f"Rolls: {str(rolls)}"
-        if len(rolls_str) < 2000:
-            await ctx.send(rolls_str)
-        await ctx.send(f"{'Total' if num_of_rolls != 1 else 'Rolled'}: {sum(rolls)}")
+        await ctx.invoke(self._roll, 12, num_of_rolls)
 
     @commands.command(name='d20')
-    async def _d20(self, ctx, *, num_of_rolls: int = 1):
+    async def _d20(self, ctx, num_of_rolls: int = 1):
         """Rolls a 20-sided dice."""
-        rolls = []
-        for i in range(num_of_rolls):
-            rolls.append(random.randint(1, 20))
-        rolls_str = f"Rolls: {str(rolls)}"
-        if len(rolls_str) < 2000:
-            await ctx.send(rolls_str)
-        await ctx.send(f"{'Total' if num_of_rolls != 1 else 'Rolled'}: {sum(rolls)}")
+        await ctx.invoke(self._roll, 20, num_of_rolls)
 
     @commands.command(name='d100')
-    async def _d100(self, ctx, *, num_of_rolls: int = 1):
+    async def _d100(self, ctx, num_of_rolls: int = 1):
         """Rolls a 100-sided dice."""
-        rolls = []
-        for i in range(num_of_rolls):
-            rolls.append(random.randint(1, 100))
-        rolls_str = f"Rolls: {str(rolls)}"
-        if len(rolls_str) < 2000:
-            await ctx.send(rolls_str)
-        await ctx.send(f"{'Total' if num_of_rolls != 1 else 'Rolled'}: {sum(rolls)}")
+        await ctx.invoke(self._roll, 100, num_of_rolls)
 
     async def cog_command_error(self, ctx: commands.Context, error: commands.CommandError):
         await ctx.send(f'An error occurred: {str(error)}')
