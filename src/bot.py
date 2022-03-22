e3c3d4b957c7354b550d17bd232413ce1837fa8e
AaronTheNerd <bargeaaron@gmail.com>
2022-03-22 14:48:37 -0400

    Merged from Replit

--- src/bot.py
+++ src/bot.py
@@ -20,21 +20,17 @@ Permission Integer: 305155152
 
 from discord.ext import commands
 
-from config import DISCORD_CONFIGS, MISC_COG_CONFIGS, SPOTIFY_COG_CONFIGS, EVENTS_COG_CONFIGS, YOUTUBE_COG_CONFIGS, DND_COG_CONFIGS, PLANK_COG_CONFIGS
+from config import DISCORD_CONFIGS, MISC_COG_CONFIGS, EVENTS_COG_CONFIGS, YOUTUBE_COG_CONFIGS, DND_COG_CONFIGS, PLANK_COG_CONFIGS
 
 from cogs.dnd_cog import DnDCog
 from cogs.events_cog import EventsCog
 from cogs.misc_cog import MiscCog
-from cogs.spotify_cog import SpotifyCog
 from cogs.youtube_cog import Music
 
 bot = commands.Bot(command_prefix=DISCORD_CONFIGS["command_prefix"], case_insensitive=DISCORD_CONFIGS["case_insensitive"])
 
 if MISC_COG_CONFIGS["enabled"]:
     bot.add_cog(MiscCog(bot))
-
-if SPOTIFY_COG_CONFIGS["enabled"]:
-    bot.add_cog(SpotifyCog(bot))
 
 if EVENTS_COG_CONFIGS["enabled"]:
     bot.add_cog(EventsCog(bot))
