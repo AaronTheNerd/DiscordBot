1ca762bd67685b9995ab9b33a0f0b1af1d25c5cb
AaronTheNerd <bargeaaron@gmail.com>
2022-11-01 08:11:05 -0400

    Restructured events cog

--- src/cogs/events.py
+++ src/cogs/events.py
@@ -1,12 +1,31 @@
+from dataclasses import dataclass, field
 import random
+from typing import Any, Dict, List
 from discord.ext import commands
 import discord
-from configs import CONFIGS
+
+
+@dataclass
+class RoleOnJoin:
+    enabled: bool
+    role: str = field(default="")
+
+
+@dataclass
+class RandomInsult:
+    enabled: bool
+    delete_after: int
+    insult_chance: float
+    adjective_chance: float
+    adjectives: List[str]
+    insults: List[str]
 
 
 class EventsCog(commands.Cog, name="Events"):
-    def __init__(self, bot):
+    def __init__(self, bot, role_on_join: Dict[str, Any], random_insult_on_command: Dict[str, Any]):
         self.bot = bot
+        self.role = RoleOnJoin(**role_on_join)
+        self.insult = RandomInsult(**random_insult_on_command)
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -14,24 +33,24 @@ class EventsCog(commands.Cog, name="Events"):
 
     @commands.Cog.listener()
     async def on_member_join(self, ctx):
-        if CONFIGS.cogs["role_on_join"].enabled:
-            role = discord.utils.get(ctx.guild.roles, name=CONFIGS.cogs["role_on_join"]["role"])
+        if self.role.enabled:
+            role = discord.utils.get(ctx.guild.roles, name=self.role.role)
             await ctx.author.add_roles(role)
 
     @commands.Cog.listener()
     async def on_command(self, ctx):
         if (
-            CONFIGS.cogs["random_insult_on_command"].enabled
-            and random.random() < CONFIGS.cogs["random_insult_on_command"]["insult_chance"]
+            self.insult.enabled
+            and random.random() < self.insult.insult_chance
         ):
             insult = f"You're a "
-            if random.random() < CONFIGS.cogs["random_insult_on_command"]["adjective_chance"]:
+            if random.random() < self.insult.adjective_chance:
                 insult += (
-                    random.choice(CONFIGS.cogs["random_insult_on_command"]["adjectives"]) + ", "
+                    random.choice(self.insult.adjectives) + ", "
                 )
-            insult += random.choice(CONFIGS.cogs["random_insult_on_command"]["insults"])
+            insult += random.choice(self.insult.insults)
             await ctx.send(
-                insult, delete_after=CONFIGS.cogs["random_insult_on_command"]["delete_after"]
+                insult, delete_after=self.insult.delete_after
             )
 
     async def cog_command_error(self, ctx: commands.Context, error: commands.CommandError):
