e7378d2668b02a6481feb4f9cb5c7222ef23034e
AaronTheNerd <bargeaaron@gmail.com>
2022-11-01 07:02:51 -0400

    Added cases for when www. is ommitted from urls

--- src/utils/search.py
+++ src/utils/search.py
@@ -13,6 +13,8 @@ class Search:
     searches: List[str] = field(default_factory=list, init=False)
     playlist: bool = field(default=False, init=False)
 
+    youtube_url: str = field(default="https://www.youtube.com", init=False)
+
     def __post_init__(self):
         self.searches = [self.search]
         try:
@@ -22,19 +24,25 @@ class Search:
         path: List[str] = parse_result.path.split("/")
         if len(path) < 2:
             return
-        if parse_result.netloc == "www.youtube.com":
+        if parse_result.netloc in ["www.youtube.com", "youtube.com"] or (
+            parse_result.netloc == "" and parse_result.path[0] in ["www.youtube.com", "youtube.com"]
+        ):
             if path[1] == "watch":
                 queries: Dict[str, str] = dict(parse_qsl(parse_result.query))
-                self.searches = [f"youtube.com/watch?v={queries['v']}"]
+                self.searches = [f"{self.youtube_url}/watch?v={queries['v']}"]
             elif path[1] == "shorts":
-                self.searches = [f"youtube.com/watch?v={path[2]}"]
+                self.searches = [f"{self.youtube_url}/watch?v={path[2]}"]
             elif path[1] == "playlist":
                 queries: Dict[str, str] = dict(parse_qsl(parse_result.query))
-                self.searches = [f"youtube.com/playlist?list={queries['list']}"]
+                self.searches = [f"{self.youtube_url}/playlist?list={queries['list']}"]
                 self.playlist = True
-        elif parse_result.netloc == "youtu.be":
-            self.searches = [f"youtube.com/watch?v={path[1]}"]
-        elif parse_result.netloc == "open.spotify.com":
+        elif parse_result.netloc == "youtu.be" or (
+            parse_result.netloc == "" and parse_result.path[0] == "youtu.be"
+        ):
+            self.searches = [f"{self.youtube_url}/watch?v={path[1]}"]
+        elif parse_result.netloc == "open.spotify.com" or (
+            parse_result.netloc == "" and parse_result.path[0] == "open.spotify.com"
+        ):
             api = SpotifyAPI(CONFIGS.spotify_token)
             if path[1] == "playlist":
                 t_resp: TrackResponse = api.get_track(path[2])
@@ -45,3 +53,27 @@ class Search:
                     f"{track['track']['name']} by {track['track']['artists'][0]['name']}"
                     for track in pl_resp.tracks["items"]
                 ]
+
+
+if __name__ == "__main__":
+    TESTS = [
+        "https://open.spotify.com/playlist/3ppjI6y3eX9WrYecZZUXWi",
+        "open.spotify.com/playlist/3ppjI6y3eX9WrYecZZUXWi",
+        "https://www.youtube.com/watch?v=c189ArVQ3k4",
+        "www.youtube.com/watch?v=c189ArVQ3k4",
+        "https://youtube.com/watch?v=c189ArVQ3k4",
+        "youtube.com/watch?v=c189ArVQ3k4",
+        "https://www.youtube.com/shorts/l4QjgKJDJjM",
+        "www.youtube.com/shorts/l4QjgKJDJjM",
+        "https://youtube.com/shorts/l4QjgKJDJjM",
+        "youtube.com/shorts/l4QjgKJDJjM",
+        "https://www.youtube.com/playlist?list=PLxA687tYuMWi8OUus77n7ZiquRq0Wlbl2",
+        "www.youtube.com/playlist?list=PLxA687tYuMWi8OUus77n7ZiquRq0Wlbl2",
+        "https://youtube.com/playlist?list=PLxA687tYuMWi8OUus77n7ZiquRq0Wlbl2",
+        "youtube.com/playlist?list=PLxA687tYuMWi8OUus77n7ZiquRq0Wlbl2",
+        "https://youtu.be/7K1KIsHp-P4"
+        "youtu.be/7K1KIsHp-P4"
+    ]
+    for test in TESTS:
+        search = Search(test)
+        print(f"Search: {search.searches}, {search.playlist}")
