3fe46359e5375afe67dc3f5b9dea12fa93ce4c7b
AaronTheNerd <bargeaaron@gmail.com>
2023-03-27 22:46:40 -0400

    Began updating to discord.py version 2.2.2

--- src/cogs/dnd.py
+++ src/cogs/dnd.py
@@ -5,73 +5,112 @@ Copyright 2022
 """
 import random
 
+from configs import CONFIGS
+
+import discord
 from discord.ext import commands
+from discord import app_commands
+from discord.app_commands import Choice
 from utils.error import on_error
 
+DICE_CHOICES = [
+    Choice(name="d4", value=4),
+    Choice(name="d6", value=6),
+    Choice(name="d8", value=8),
+    Choice(name="d10", value=10),
+    Choice(name="d12", value=12),
+    Choice(name="d20", value=20),
+    Choice(name="d100", value=100)
+]
 
-class DnDCog(commands.Cog, name="DnD"):
+class DnDCog(commands.Cog):
     def __init__(self) -> None:
         pass
 
-    @commands.command(name="roll")
-    async def _roll(self, ctx: commands.Context, num_of_sides: int, num_of_rolls: int = 1) -> None:
-        """Rolls a dice."""
-        rolls = []
-        for _ in range(num_of_rolls):
-            rolls.append(random.randint(1, num_of_sides))
-        rolls_str = f"Rolls: {str(rolls)}"
-        if len(rolls_str) < 2000 and num_of_rolls != 1:
-            await ctx.send(rolls_str)
-        await ctx.send(f"{'Total' if num_of_rolls != 1 else 'Rolled'}: {sum(rolls)}")
+    async def _roll(self, interaction: discord.Interaction, sides: int, rolls: int = 1) -> None:
+        all_rolls = []
+        for _ in range(rolls):
+            all_rolls.append(random.randint(1, sides))
+        rolls_str = f"Rolls: {str(all_rolls)}"
+        if len(rolls_str) < 2000 and rolls != 1:
+            await interaction.response.send_message(rolls_str)
+        await interaction.response.send_message(f"{'Total' if rolls != 1 else 'Rolled'}: {sum(all_rolls)}")
 
-    @commands.command(name="d4")
-    async def _d4(self, ctx: commands.Context, num_of_rolls: int = 1) -> None:
-        """Rolls a 4-sided dice."""
-        await ctx.invoke(self._roll, 4, num_of_rolls)
+    @app_commands.command(
+        name="d4",
+        description="Rolls some D4."
+    )
+    async def _d4(self, interaction: discord.Interaction, rolls: int = 1) -> None:
+        await self._roll(interaction, 4, rolls)
 
-    @commands.command(name="d6")
-    async def _d6(self, ctx: commands.Context, num_of_rolls: int = 1) -> None:
-        """Rolls a 6-sided dice."""
-        await ctx.invoke(self._roll, 6, num_of_rolls)
+    @app_commands.command(
+        name="d6",
+        description="Rolls some D6."
+    )
+    async def _d6(self, interaction: discord.Interaction, rolls: int = 1) -> None:
+        await self._roll(interaction, 6, rolls)
 
-    @commands.command(name="d8")
-    async def _d8(self, ctx: commands.Context, num_of_rolls: int = 1) -> None:
-        """Rolls an 8-sided dice."""
-        await ctx.invoke(self._roll, 8, num_of_rolls)
+    @app_commands.command(
+        name="d8",
+        description="Rolls some D8."
+    )
+    async def _d8(self, interaction: discord.Interaction, rolls: int = 1) -> None:
+        await self._roll(interaction, 8, rolls)
 
-    @commands.command(name="d10")
-    async def _d10(self, ctx: commands.Context, num_of_rolls: int = 1) -> None:
-        """Rolls a 10-sided dice."""
-        await ctx.invoke(self._roll, 10, num_of_rolls)
+    @app_commands.command(
+        name="d10",
+        description="Rolls some D10."
+    )
+    async def _d10(self, interaction: discord.Interaction, rolls: int = 1) -> None:
+        await self._roll(interaction, 10, rolls)
 
-    @commands.command(name="d12")
-    async def _d12(self, ctx: commands.Context, num_of_rolls: int = 1) -> None:
-        """Rolls a 12-sided dice."""
-        await ctx.invoke(self._roll, 12, num_of_rolls)
+    @app_commands.command(
+        name="d12",
+        description="Rolls some D12."
+    )
+    async def _d12(self, interaction: discord.Interaction, rolls: int = 1) -> None:
+        await self._roll(interaction, 12, rolls)
 
-    @commands.command(name="d20")
-    async def _d20(self, ctx: commands.Context, num_of_rolls: int = 1) -> None:
-        """Rolls a 20-sided dice."""
-        await ctx.invoke(self._roll, 20, num_of_rolls)
+    @app_commands.command(
+        name="d20",
+        description="Rolls some D20."
+    )
+    async def _d20(self, interaction: discord.Interaction, rolls: int = 1) -> None:
+        await self._roll(interaction, 20, rolls)
 
-    @commands.command(name="d100")
-    async def _d100(self, ctx: commands.Context, num_of_rolls: int = 1) -> None:
-        """Rolls a 100-sided dice."""
-        await ctx.invoke(self._roll, 100, num_of_rolls)
+    @app_commands.command(
+        name="d100",
+        description="Rolls some D100."
+    )
+    async def _d100(self, interaction: discord.Interaction, rolls: int = 1) -> None:
+        await self._roll(interaction, 100, rolls)
 
-    @commands.command(name="rolladv", aliases=["rolla", "rolladvantage", "ra"])
-    async def _rolladv(self, ctx: commands.Context, num_of_sides: int) -> None:
-        """Rolls with advantage."""
-        roll1, roll2 = random.randint(1, num_of_sides), random.randint(1, num_of_sides)
+    @app_commands.command(
+        name="rolladv",
+        description="Rolls with advantage."
+    )
+    @app_commands.choices(sides = DICE_CHOICES)
+    async def _rolladv(self, interaction: discord.Interaction, sides: int) -> None:
+        roll1, roll2 = random.randint(1, sides), random.randint(1, sides)
         roll = roll1 if roll1 > roll2 else roll2
-        await ctx.send(f"Rolled with Advantage: {roll}")
+        await interaction.response.send_message(f"Rolled with Advantage: {roll}")
 
-    @commands.command(name="rolldis", aliases=["rolld", "rolldisadvantage", "rd"])
-    async def _rolldis(self, ctx: commands.Context, num_of_sides: int) -> None:
+    @app_commands.command(
+        name="rolldis",
+        description="Rolls with advantage."
+    )
+    @app_commands.choices(sides = DICE_CHOICES)
+    async def _rolldis(self, interaction: discord.Interaction, num_of_sides: int) -> None:
         """Rolls with disadvantage."""
         roll1, roll2 = random.randint(1, num_of_sides), random.randint(1, num_of_sides)
         roll = roll1 if roll1 < roll2 else roll2
-        await ctx.send(f"Rolled with Disadvantage: {roll}")
+        await interaction.response.send_message(f"Rolled with Disadvantage: {roll}")
 
     async def cog_command_error(self, ctx: commands.Context, error: commands.CommandError) -> None:
         await on_error(ctx, error, None)
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(
+        DnDCog(**CONFIGS.cogs.dnd.kwargs),
+        guilds=[discord.Object(id=CONFIGS.guild_id)]
+    )
